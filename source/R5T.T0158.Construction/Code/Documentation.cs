c4ac4698b3578124fcf44a8195e503789814a542
davidcoats <david@getsafetycone.com>
2023-02-14 13:02:38 -0800

    Initial commit.

---
+++ source/R5T.T0158.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0158.Construction
+{
+	/// <summary>
+	/// Construction console project for the R5T.T0158 library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0158/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0158
+{
+	/// <summary>
+	/// IHumanOutput types library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
